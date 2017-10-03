03836c329bdde766a6628201bcdd2df0092b3c00
yarynayavor <yourhurt09@gmail.com>
2017-10-04 00:46:15 +0300

    Added all files with validating form and created table

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var userArr=[];
+
+function User(name,sex,birthDate,address,phone,email) {
+	this.name=name;
+	this.sex=sex;
+	this.birthDate=birthDate;
+	this.address=address;
+	this.phone=phone;
+	this.email=email;
+}
+
+User.prototype.getUserInTable=function () {
+ 
+}
