e8f5a4689749f13ecb58dcc4c4e93cb5ce4832df
yarynayavor <yourhurt09@gmail.com>
2017-10-05 01:09:49 +0300

    added function which added to table user data and some another functions

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
-var userArr=[];
+function SuperUser() {
+	this.changeDataVisibility=function() {
+		//in progress
+	}
+}
 
 function User(name,sex,birthDate,address,phone,email) {
 	this.name=name;
@@ -7,8 +11,71 @@ function User(name,sex,birthDate,address,phone,email) {
 	this.address=address;
 	this.phone=phone;
 	this.email=email;
+
+	this.isDataVisible=true;
 }
 
-User.prototype.getUserInTable=function () {
- 
+User.prototype = new SuperUser();
+
+var userArr=[];
+
+User.prototype.addUser=function (){ 
+	var tableId=document.getElementById('table');
+	var tableRow=tableId.insertRow(1);
+	tableRow.classList.add('data');
+	var cell1 = tableRow.insertCell(0);
+	var cell2 = tableRow.insertCell(1);
+	var cell3 = tableRow.insertCell(2);
+	var cell4 = tableRow.insertCell(3);
+	var cell5 = tableRow.insertCell(4);
+	var cell6 = tableRow.insertCell(5);
+	cell1.textContent = this.name;
+	cell2.textContent = this.sex;
+	cell3.textContent = this.birthDate;
+	cell4.textContent = this.address;
+	cell5.textContent = this.phone;
+	cell6.textContent = this.email;
 }
+
+function resetForm() {
+	var form=document.getElementById('form');
+	form.reset();
+}
+
+function checkDublicates(p,e) {
+	for (var i = 0; i < userArr.length; i++) {
+		if((userArr[i].phone.toLowerCase()==p.toLowerCase()) ||
+			(userArr[i].email.toLowerCase()==e.toLowerCase())) {
+			return true;
+	    }
+    }
+	return false;
+}
+
+$('input[type="submit"]').click(function (e) {
+
+	nm=/^[^\d]*$/;
+	pm=/[\+]\d{2}[\(]\d{3}[\)]\d{3}[\-]\d{2}[\-]\d{2}/;
+	em=/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/;
+
+	var name=document.getElementById('name').value;
+	var sex=document.getElementById('sex').value;
+	var date=document.getElementById('date').value;
+	var address=document.getElementById('address').value;
+	var phone=document.getElementById('phone').value;
+	var email=document.getElementById('email').value;
+
+	if(((name.match(nm))&&(name)) && (sex) && (date) && (address) && ((phone.match(pm))&&phone) && ((email.match(em))&&email)) {
+		e.preventDefault();
+		if(checkDublicates(phone,email)) {
+			alert("Sorry. This user already exist in our system. \n\
+				Write another pnone number or/and email!");
+		} else {
+			var newInstanses=new User(name,sex,date,address,phone,email);
+			userArr.push(newInstanses);
+			userArr[userArr.length-1].addUser();
+			resetForm();
+		}
+		//console.log(userArr[userArr.length-1].length);
+	}
+});
